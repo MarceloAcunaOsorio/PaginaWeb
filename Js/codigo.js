901c42d0b8941e4e2c1a724d9b1fe9a43df83074
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-05-19 21:58:31 -0400

    se cuadra el header y el footer

---
+++ Js/codigo.js
@@ -0,0 +1,54 @@
+
+
+//Función para cargar el menú
+    function cargarMenu() 
+    {
+        fetch('/menu.html')
+            .then(response => response.text())
+            .then(data => {
+                // Insertar el contenido del menú en el contenedor
+                document.getElementById('menuContainer').innerHTML = data;
+            })
+            .catch(error => {
+                console.error('Error al cargar el menú:', error);
+            });
+    }
+
+    // Llamar a la función para cargar el menú cuando la página cargue
+    window.addEventListener('DOMContentLoaded', cargarMenu);
+
+
+
+
+
+
+    
+//Función para cargar el footer
+function cargarfooter() 
+{
+    fetch('/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            // Insertar el contenido del menú en el contenedor
+            document.getElementById('footerContainer').innerHTML = data;
+        })
+        .catch(error => {
+            console.error('Error al cargar el footer:', error);
+        });
+}
+
+// Llamar a la función para cargar el menú cuando la página cargue
+window.addEventListener('DOMContentLoaded', cargarfooter);
+
+
+//cargar paginas al momento de hacer click en menu
+
+function cargarPagina(url) {
+    fetch(url)
+    .then(response => response.text())
+    .then(html => {
+        document.getElementById('contenido').innerHTML = html;
+    })
+    .catch(error => console.error('Error al cargar la página:', error));
+}
+
