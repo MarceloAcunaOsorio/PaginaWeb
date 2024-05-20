db204213ee7f425460c0e202c1356e3e71d8d468
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-05-20 14:42:23 -0400

    se elimina footer y menu, se agregan al index

--- Js/codigo.js
+++ Js/codigo.js
@@ -1,50 +1,5 @@
 
 
-//Función para cargar el menú
-    function cargarMenu() 
-    {
-        fetch('/menu.html')
-            .then(response => response.text())
-            .then(data => {
-                // Insertar el contenido del menú en el contenedor
-                document.getElementById('menuContainer').innerHTML = data;
-            })
-            .catch(error => {
-                console.error('Error al cargar el menú:', error);
-            });
-    }
-
-    // Llamar a la función para cargar el menú cuando la página cargue
-    window.addEventListener('DOMContentLoaded', cargarMenu);
-
-
-
-
-
-
-    
-//Función para cargar el footer
-function cargarfooter() 
-{
-    fetch('/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            // Insertar el contenido del menú en el contenedor
-            document.getElementById('footerContainer').innerHTML = data;
-        })
-        .catch(error => {
-            console.error('Error al cargar el footer:', error);
-        });
-}
-
-// Llamar a la función para cargar el footer cuando la página cargue
-window.addEventListener('DOMContentLoaded', cargarfooter);
-
-
-
-
-
-
 
 //cargar paginas al momento de hacer click en menu
 
