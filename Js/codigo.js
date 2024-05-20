a3cf9096754aef886e7aa89bfb8994aa378f06c3
MarceloAcunaOsorio <163946987+MarceloAcunaOsorio@users.noreply.github.com>
2024-05-20 15:52:19 -0400

    se corrige direcci0on de paginas al realizar clic

--- Js/codigo.js
+++ Js/codigo.js
@@ -4,9 +4,12 @@
 //cargar paginas al momento de hacer click en menu
 
 function cargarPagina(url) {
+    
     fetch(url)
     .then(response => response.text())
     .then(html => {document.getElementById('contenido').innerHTML = html;})
     .catch(error => console.error('Error al cargar la página:', error));
 }
+
+
 
